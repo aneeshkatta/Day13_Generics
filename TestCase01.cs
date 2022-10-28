f2089719d028f6fedda98a374d3431c66caad23f
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-28 07:38:29 +0530

    Add files via upload
    added uc2

---
+++ TestCase01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatMaxcheckGenerics
+{
+    internal class TestCase01
+    {
+        public static void Maxf1()
+        {
+            Console.WriteLine("Find max Number");
+
+            double doubleoutput = MaximumFloatCheck.MaximumFloatNumber(91.52, 22.95, 77.56);
+            Console.WriteLine(doubleoutput);
+
+        }
+    }
+}
