5a5adbf656c0abb372c2f86057194e568683732a
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-28 07:37:44 +0530

    Add files via upload
    added uc1

---
+++ TestCase01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumberProject1
+{
+    internal class TestCase01
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Find max Number");
+            int output = MaximumNumberCheck.MaximumIntegerNumber1(99, 22, 33);
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ UC1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumberProject1
+{
+    class UC1
+    {
+        
+
+            public static void Main(string[] args)
+            {
+                
+                Console.WriteLine("Enter The First Number");
+                int firstValue = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter The Second Number");
+                int secondValue = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter The Third Number");
+                int thirdValue = Convert.ToInt32(Console.ReadLine());
+
+               MaximumNumberCheck.MaximumIntegerNumber1(firstValue, secondValue, thirdValue);
+
+            }
+
+        
+    }
+}
+
+
