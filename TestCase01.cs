78a18bb4ec86871240660d47d0f19837d4e85a9f
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-28 07:40:52 +0530

    Add files via upload
    added uc3

---
+++ TestCase01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMaxCheckGenerics
+{
+    internal class TestCase01
+    {
+        public static void First()
+        {
+
+            string strigoutput = MaxStringCheck.MaximumStringNumber("Z", "R", "A");
+            Console.WriteLine("input :", "Z", "R", "A");
+            Console.WriteLine(strigoutput);
+            Console.ReadKey();
+        }
+
+
+      
+    }
+    
+}
